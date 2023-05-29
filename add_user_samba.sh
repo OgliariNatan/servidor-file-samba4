a5af233793c6bab88b2048d9668696dd1c278491
OgliariNatan <natanogliari@gmail.com>
2023-05-29 11:04:18 -0300

    scripty, concluido falta testar

--- add_user_samba.sh
+++ add_user_samba.sh
@@ -11,10 +11,36 @@
 #########################Torne o scripty executavél ##########
 ## chmod u+x novo_script
 ##############################
-echo "Bem vindo a criação de usuários samba"
-echo "Digite o nome do usuário:"
-read new_user
+echo "Bem vindo a criação ou remoção de usuários samba"
+echo "Para Adicionar um usuário digite (1)."
+echo "Para alterar uma SENHA de um usuário digite (2)."
+echo "Para Remover um usuário digite (3)."
+read id_status
 
-useradd $new_user #Cria usuario LINUX
-echo "Digite uma senha para o: $new_user"
-smbpasswd -a $new_user #Associa user ao samba e especifica uma senha
+case "$id_status" in
+  1)
+    echo "Escolheu a incerção de um novo usuário."
+    echo "Digite o nome do usuário:"
+    read new_user
+    useradd $new_user #Cria usuario LINUX
+    echo "Digite uma senha para o: $new_user"
+    smbpasswd -a $new_user #Associa user ao samba e especifica uma senha
+;;
+  2)
+    echo "Escolheu a alteração de senha."
+    echo "Usuarios cadastrados."
+    pdbedit -L
+    echo "Digite o usuário a ser redifinido a senha."
+    read user_new_passwd
+    smbpasswd -U $user_new_passwd
+;;
+  3)
+    echo "Escolheu a remoção de usuários."
+    echo "Usuarios cadastrados."
+    pdbedit -L
+    echo  "Digite o usuário a ser removido."
+    read remov_user
+    smbpasswd -x $remov_user
+    userdel -r $remov_user
+esac
+echo "Concluido"
