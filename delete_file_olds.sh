67d7e6c95adea92a8b318b590cf21afeae859c61
OgliariNatan <natanogliari@gmail.com>
2023-06-07 10:44:51 -0300

    atualizado o delete_filr_olds.sh, lista os arquivos

--- delete_file_olds.sh
+++ delete_file_olds.sh
@@ -15,14 +15,20 @@ echo -e "Entro no scripty de remoção de arquivos da lixeira \n\n"
 cd /home/servidor/Área\ de\ Trabalho/lixeira
 
 echo	"Mudou de dir"
-
+echo -e "lista de arquivos: \t $(find . -type f -mtime +60)"
 sudo find . -type f -mtime +90 -delete
+#Não pode possuir espaço entre a vbariavél é o valor: var=val
+ver_remocao=$?
+echo  -e " este é o retorno: \t $ver_remocao \t aqui"
+#echo "REMOVEU......."
+
 echo -e "Antecede os if \n ...."
- if ($? -ne 0); then
+ if ($ver_remocao == 0)
+  then
 	echo -e	"ENtrou no if \n ...."
 	echo "Foi apagado arquivos da lixeira" >> /home/servidor/Documentos/corpo_da_mensagem.txt
   else
-    echo "Não foi apagado arquivos da lixeira" >> /home/servidor/Documentos/corpo_da_mensagem.txt
+    echo -e "Não foi apagado arquivos da lixeira \t o retorno do comando é: $ver_remocao" >> /home/servidor/Documentos/corpo_da_mensagem.txt
  fi
 
 echo "Fim do scripty de remoção da lixeira"
