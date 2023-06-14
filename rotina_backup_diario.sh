5ce016b39d1a0ba368b95102961168eacc2a03b2
OgliariNatan <natanogliari@gmail.com>
2023-06-14 11:19:26 -0300

    Add uma rotina de backup diario apenas de arquivos alterados ou adicionados

--- rotina_backup_diario.sh
+++ rotina_backup_diario.sh
@@ -21,7 +21,7 @@
 ##
 ##Opção: [v] exibe o progresso, [p] mantem as permissões
 echo -e "Inicializado o backup diario" $(date +%d%m%y--%H:%M) >> /home/servidor/Documentos/corpo_da_mensagem.txt
-#tar -uzf /home/servidor/Documentos/win/backup_diario.tar.gz /home/servidor/Área\ de\ Trabalho/Compartilhamento
+tar -uzf /home/servidor/Documentos/win/backup_diario.tar.gz /home/servidor/Área\ de\ Trabalho/Compartilhamento
 #echo -e $(ls -lth /home/Documentos/win) >> /home/servidor/Documentos/corpo_da_mensagem.txt
 echo -e "Finalizado o backup diario\n" >> /home/servidor/Documentos/corpo_da_mensagem.txt
 echo -e "################################"
