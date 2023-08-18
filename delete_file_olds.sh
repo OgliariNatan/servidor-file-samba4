c9dd82470974bc3998b0a7c0cb65ed7ccea58dba
OgliariNatan <natanogliari@gmail.com>
2023-08-18 10:29:51 -0300

    update delete_file_old.sh

--- delete_file_olds.sh
+++ delete_file_olds.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #
 #####	NOME:				delete_file_olds.sh
-#####	VERSÃO:				1.1
+#####	VERSÃO:				1.2
 #####	DESCRIÇÃO:			Implementação rotina de remoção de arquivos com mais de 90dias da lixeira
+##### e cria um registro de log para este scripty
 #####	DATA DA CRIAÇÃO:	26/05/2023
+##### ATUALIZADO EM: 18/08/2023
 #####	ESCRITO POR:		Natan Ogliari
 #####	E-MAIL:				natanogliari@gmail.com
 #####	DISTRO:				Ubuntu GNU/Linux 22.04
 #####	LICENÇA:			MIT license
 #####	PROJETO:			https://github.com/OgliariNatan/servidor-file-samba4
 
-echo -e "Entro no scripty de remoção de arquivos da lixeira \n\n"
+echo -e "Removendo arquivos na data de: $(date +%d%m%y)" >> /home/servidor/backup/log/log.delete_file_olds
+echo -e "Entro no scripty de remoção de arquivos da lixeira \n\n" >> /home/servidor/backup/log/log.delete_file_olds
 
-cd /home/servidor/Área\ de\ Trabalho/lixeira
+cd /home/servidor/backup/lixeira
 
 echo	"Mudou de dir"
 echo -e "lista de arquivos: \t $(find . -type f -mtime +90)"
@@ -25,9 +28,9 @@ echo  -e " este é o retorno: \t $? \t aqui"
 echo -e "Antecede os if \n ...."
  if [ $? == 0 ]; then
 	#echo -e	"Entrou no if \n ...."
-	echo -e "Foi apagado arquivos da lixeira." >> /home/servidor/Documentos/corpo_da_mensagem.txt
+	echo -e "Foi apagado arquivos da lixeira." >> /home/servidor/backup/log/log.delete_file_olds
   else
-    echo -e "Não foi apagado arquivos da lixeira \t o retorno do comando é: $?" >> /home/servidor/Documentos/corpo_da_mensagem.txt
+    echo -e "Não foi apagado arquivos da lixeira \t o retorno do comando é: $?" >> /home/servidor/backup/log/log.delete_file_olds
  fi
 
-echo "Fim do scripty de remoção da lixeira"
+echo "Fim do scripty de remoção da lixeira" >> /home/servidor/backup/log/log.delete_file_olds
