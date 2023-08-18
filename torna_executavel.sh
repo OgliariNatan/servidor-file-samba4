d262c8fe2a1793ed128bcfaba0b3b8bbe9c3411c
OgliariNatan <natanogliari@gmail.com>
2023-08-18 10:34:18 -0300

    scripty para tornar executavel

---
+++ torna_executavel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#####	NOME:				torna_executavel.sh
+#####	VERSÃO:				1.2
+#####	DESCRIÇÃO:			Implementação de rotina para tornar aqruivos executaveis
+#####	DATA DA CRIAÇÃO:	18/08/2023
+##### ATUALIZADO EM: 18/08/2023
+#####	ESCRITO POR:		Natan Ogliari
+#####	E-MAIL:				natanogliari@gmail.com
+#####	DISTRO:				Ubuntu GNU/Linux 22.04
+#####	LICENÇA:			MIT license
+#####	PROJETO:			https://github.com/OgliariNatan/servidor-file-samba4
+
+
+cd /home/servidor/backup/diario/servidor-file-samba4
+chmod +x rotina_backup_diario.sh delete_file_olds.sh rotina_backup.sh
