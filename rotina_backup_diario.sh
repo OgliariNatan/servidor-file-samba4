9b268ca2cb7bcdd5c04bb1e2dae8614134145024
Natan Ogliari <natanogliari@gmail.com>
2024-09-04 10:16:36 -0300

    Update rotina_backup_diario.sh

--- rotina_backup_diario.sh
+++ rotina_backup_diario.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 ##
-#####	NOME:				rotina_backup_diario.sh
-#####	VERSÃO:				0.1
-#####	DESCRIÇÃO:			Implementação rotina de backup diario com arquivos modificados
-#####	DATA DA CRIAÇÃO:		14/06/2023
-##### ATUALIZADO EM: 18/08/2023
-#####	ESCRITO POR:			Natan Ogliari
-#####	E-MAIL:				natanogliari@gmail.com
-#####	DISTRO:				Ubuntu GNU/Linux 22.04
-#####	LICENÇA:			MIT license
-#####	PROJETO:			https://github.com/OgliariNatan/servidor-file-samba4
-#########################Torne o scripty executavél ##########
+#####   NOME:                           rotina_backup_diario.sh
+#####   VERSÃO:                         0.1
+#####   DESCRIÇÃO:                      Implementação rotina de backup diario com arquivos modificados
+#####   DATA DA CRIAÇÃO:                14/06/2023
+#####   ATUALIZADO EM:                  04/09/2024
+#####   ESCRITO POR:                    Natan Ogliari
+#####   E-MAIL:                         natanogliari@gmail.com
+#####   DISTRO:                         Ubuntu GNU/Linux 24.04
+#####   LICENÇA:                        MIT license
+#####   PROJETO:                        https://github.com/OgliariNatan/servidor-file-samba4
+#########################Torne o script executável ##########
 ## chmod +x novo_script
 ##############################
 ## Script idealizado para manter uma política de backup dos arquivos
@@ -20,9 +20,16 @@
 ## a compactação dos arquivos compartilhados e já os coloca na pasta
 ## montada referente ao servidor de backup.
 ##
-##Opção: [v] exibe o progresso, [p] mantem as permissões
-echo -e "Inicializado o backup diario" $(date +%d%m%y--%H:%M) >> /home/servidor/backup/log/corpo_da_mensagem.txt
-tar -czf -G /home/servidor/backup/diario/backup_diario.tar.gz /home/servidor/Compartilhados
-#echo -e $(ls -lth /home/Documentos/win) >> /home/servidor/Documentos/corpo_da_mensagem.txt
-echo -e "Finalizado o backup diario\n" >> /home/servidor/backup/log/corpo_da_mensagem.txt
+## Opção: [v] exibe o progresso, [p] mantem as permissões
+echo -e "Inicializado o backup diario" $(date +%d%m%y) >> /home/servidor/samba/conf/log/log.bk_diario
+
+# Data de ontem
+DATA=$(date -d "yesterday" '+%Y-%m-%d')
+
+# Criar o backup apenas dos arquivos modificados no dia anterior
+#find /home/servidor/samba/data/compartilhados -type f -newermt "$DATA" -print0 | tar --null --files-from=- --no-recursion -czvf /home/servidor/samba/bk_diario/$(date +%d%m%y)_backup_diario.tar.gz
+
+find /home/servidor/samba/data/compartilhados -type f -newermt "$DATA" -print0 | tar --null -czvf /home/servidor/samba/bk_diario/$(date +%d%m%y)_backup_diario.tar.gz --files-from=-
+
+echo -e "Finalizado o backup diario\n" >> /home/servidor/samba/conf/log/log.bk_diario
 echo -e "################################"
