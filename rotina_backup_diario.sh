f7183d9580353c83c02eeed492880efd01ea99e2
Natan Ogliari <natanogliari@gmail.com>
2024-09-19 10:28:32 -0300

    Update rotina_backup_diario.sh

--- rotina_backup_diario.sh
+++ rotina_backup_diario.sh
@@ -2,12 +2,11 @@
 ##
 #####   NOME:                           rotina_backup_diario.sh
 #####   VERSÃO:                         0.1
-#####   DESCRIÇÃO:                      Implementação rotina de backup diario com arquivos modificados
-#####   DATA DA CRIAÇÃO:                14/06/2023
-#####   ATUALIZADO EM:                  04/09/2024
+#####   DESCRIÇÃO:                      Implementação rotina de backup diario com arquivos modificados#####   DATA DA CRIAÇÃO:                14/06/2023
+#####   ATUALIZADO EM:                  18/08/2023
 #####   ESCRITO POR:                    Natan Ogliari
 #####   E-MAIL:                         natanogliari@gmail.com
-#####   DISTRO:                         Ubuntu GNU/Linux 24.04
+#####   DISTRO:                         Ubuntu GNU/Linux 22.04
 #####   LICENÇA:                        MIT license
 #####   PROJETO:                        https://github.com/OgliariNatan/servidor-file-samba4
 #########################Torne o script executável ##########
@@ -27,9 +26,15 @@ echo -e "Inicializado o backup diario" $(date +%d%m%y) >> /home/servidor/samba/c
 DATA=$(date -d "yesterday" '+%Y-%m-%d')
 
 # Criar o backup apenas dos arquivos modificados no dia anterior
-#find /home/servidor/samba/data/compartilhados -type f -newermt "$DATA" -print0 | tar --null --files-from=- --no-recursion -czvf /home/servidor/samba/bk_diario/$(date +%d%m%y)_backup_diario.tar.gz
+#find /home/servidor/samba/data/compartilhados -type f -newermt "$DATA" -print0 | tar --null --files->
 
-find /home/servidor/samba/data/compartilhados -type f -newermt "$DATA" -print0 | tar --null -czvf /home/servidor/samba/bk_diario/$(date +%d%m%y)_backup_diario.tar.gz --files-from=-
+find /home/servidor/samba/data/compartilhados -type f -newermt "$DATA" -print0 | tar --null -czvf /ho>
+#Remove arquivos antigos
+
+echo -e "Removendo o arquivo\t" $(find /home/servidor/samba/bk_diario/ -mtime +7) >> /home/servidor/s>
+
+rm $(find /home/servidor/samba/bk_diario/ -mtime +7)
+
 
 echo -e "Finalizado o backup diario\n" >> /home/servidor/samba/conf/log/log.bk_diario
 echo -e "################################"
